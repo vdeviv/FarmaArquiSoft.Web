35b7fb88ac143f315c3f4078c7219898b2cfb28b
TheEduMarin <168266692+TheEduMarin@users.noreply.github.com>
2025-11-19 23:35:54 -0400

    Delete FarmaArquiSoft.Web/DTOs/ProviderDTO.cs

--- FarmaArquiSoft.Web/DTOs/ProviderDTO.cs
+++
@@ -1,9 +0,0 @@
-
-namespace FarmaArquiSoft.Web.DTOs;
-public class ProviderDTO {
-    public int Id{get;set;}
-    public string FirstName{get;set;}
-    public string LastName{get;set;}
-    public string? Email{get;set;}
-    public string? Phone{get;set;}
-}
